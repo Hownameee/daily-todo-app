858ef75899d77d8eed2ba52408d75262abbdc542
Hownameee <namhaohuynh@gmail.com>
2025-11-13 16:53:58 +0700

    add env

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,8 +6,8 @@ export default class Config {
 	public readonly MongoUri: string;
 
 	constructor() {
-		this.jwtSecret = randomBytes(64).toString("hex");
-		this.PORT = 4000;
+		this.jwtSecret = process.env.JWTSECRET || randomBytes(64).toString("hex");
+		this.PORT = Number(process.env.PORT) || 4000;
 		this.MongoUri = process.env.MONGOURI ?? "mongodb://127.0.0.1:30000/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.9";
 	}
 }
